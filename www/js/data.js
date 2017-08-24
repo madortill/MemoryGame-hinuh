27ec18575d035ef04d786caf6d805a8bebd8cc48
Oran <oran1284@gmail.com>
2017-08-24 11:10:09 +0300

    init commit

---
+++ www/js/data.js
@@ -0,0 +1,22 @@
+// (function () {
+// angular.module('MemoryGame', ['ionic'])
+/* the "Cards"  factory is the value of the term and the definition of the card
+
+*/
+
+
+
+//   .factory('Cards', function () {
+//       // Might use a resource here that returns a JSON array
+
+//       // Some fake testing data
+//       var  = [{
+//         term: "  ",
+//         definition:"  ",
+
+//       }, {
+//         // id: 1,
+//         // name: 'Max Lynx',
+//         // lastText: 'Hey, it\'s me',
+//         // face: 'img/max.png'
+//       }, {}];
